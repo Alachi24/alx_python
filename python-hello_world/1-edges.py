f50216b602fbb0252f8d7889075bf9296ea98c54
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-18 18:50:18 +0100

    updated variable

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# word = "Holberton"
+# # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# print("First 3 letters: {}".format(word[0:3]))
+# print("Last 2 letters: {}".format(word[7:9]))
+# print("Middle word: {}".format(word[1:8]))
+
+word = "School"
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:9]))
-print("Middle word: {}".format(word[1:8]))
+print("Last 2 letters: {}".format(word[4:6]))
+print("Middle word: {}".format(word[1:5]))
