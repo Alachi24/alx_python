093ed7965bfb07394bdfed98deb8a873a1dce6d5
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-20 11:00:25 +0100

    modified ver

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,37 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-a = "last digit of"
-b = "is"
-c = "and is greater than 5"
-d = "and is 0"
-e = "and is less than 6 and not 0"
-f = abs(number) % 10
 
+
+last_digit = abs(number) % 10
 if number < 0:
-    f = -f
-    print("{} {} {} {} ".format(a, number, b, f,))
+    last_digit = -last_digit
+    print("last digit of", number, "is", last_digit, end=" ")
 else:
-    print("{} {} {} {} ".format(a, number, b, f,))
+    print("last digit of", number, "is", last_digit, end=" ")
 
 if number > 5:
-    print("{} {} {} {} and is greater than 5".format(a, number, b, f))
+    print("and is greater than 5")
 elif number == 0:
-    print("{} {} {} {} and is 0".format(a, number, b, f))
+    print("and is 0")
 else:
-    print("{} {} {} {} {}".format(a, number, b, f, e))
-
-
-# last_digit = abs(number) % 10
-# if number < 0:
-#     last_digit = -last_digit
-#     print("last digit of", number, "is", last_digit, end=" ")
-# else:
-#     print("last digit of", number, "is", last_digit, end=" ")
-
-#     if number > 5:
-#         print("and is greater than 5")
-#     elif number == 0:
-#         print("and is 0")
-#     else:
-#         print("and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
