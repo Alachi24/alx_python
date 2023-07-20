5f07b5e1480803609887f14d173d00f67a05a1f5
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-21 00:02:50 +0100

    task 6 achieved

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,9 +1,9 @@
 def generate_combinations():
-    combinations = [(i, j) for i in range(10) for j in range(10) if i != j]
+    combinations = [(i, j) for i in range(89) for j in range(89) if i != j]
     return combinations
 
 
 all_combinations = generate_combinations()
 
 for combinations in all_combinations:
-    print(f"{combinations[0]}, {combinations[1]}")
+    print(f"{combinations[0]}, {combinations[1]}", end=", ")
