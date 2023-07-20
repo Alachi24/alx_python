a92bd98d89457b1252da24a549ea65feb73a5014
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-20 10:45:25 +0100

    modified task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,14 +8,32 @@ c = "and is greater than 5"
 d = "and is 0"
 e = "and is less than 6 and not 0"
 f = abs(number) % 10
+g = end = " "
 
 if number < 0:
     f = -f
-    print("{} {} {} {}".format(a, number, b, f,))
+    print("{} {} {} {} {}".format(a, number, b, f, g))
+else:
+    print("{} {} {} {} {}".format(a, number, b, f, g))
 
-if abs(number) % 10 > 5:
+if number > 5:
     print("{} {} {} {} and is greater than 5".format(a, number, b, f))
-if abs(number) % 10 == 0:
+elif number == 0:
     print("{} {} {} {} and is 0".format(a, number, b, f))
-if abs(number) % 10 < 6 != 0:
+else:
     print("{} {} {} {} {}".format(a, number, b, f, e))
+
+
+# last_digit = abs(number) % 10
+# if number < 0:
+#     last_digit = -last_digit
+#     print("last digit of", number, "is", last_digit, end=" ")
+# else:
+#     print("last digit of", number, "is", last_digit, end=" ")
+
+#     if number > 5:
+#         print("and is greater than 5")
+#     elif number == 0:
+#         print("and is 0")
+#     else:
+#         print("and is less than 6 and not 0")
