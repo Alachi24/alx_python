5c9e733b219f2efc28c346cf9c1a4522afde9491
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-21 11:28:40 +0100

    task 6 achieved

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,7 @@
-for i in range(1, 9):
-    for k in range(1, 9):
-        if i != k:
-            print(str(i)+str(k))
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:02d}, ".format(i * 10 + j))
+        else:
+            print("{:02d}, ".format(i * 10 + j), end="")
