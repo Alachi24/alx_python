7f5eee976598d884020d8b1c28285a0f5207ced2
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-22 15:45:34 +0100

    task 0 modification

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b
 
 
-a = [1, 98, 100]
-b = [2, 0, -2]
-
-print(" {}".format(a[0] + b[0]))
+if __name__ == "__main__":
+    print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
