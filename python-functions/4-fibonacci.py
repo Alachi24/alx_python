0726879c6240f87a2b37211f517f41cb2f19c9bc
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-23 05:25:33 +0100

    All Task completed

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fib_sequence = [0]
+
+    if n == 1:
+        return fib_sequence
+
+    fib_sequence.append[1]
+
+    while len(fib_sequence) < n:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+
+        return fib_sequence
