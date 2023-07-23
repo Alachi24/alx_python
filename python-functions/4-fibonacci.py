e32913c680403a0746c7d08296dad5460af1b558
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-23 05:50:20 +0100

    modifications on task 4 & 5

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,16 +1,23 @@
 def fibonacci_sequence(n):
     if n <= 0:
         return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_numbers = [0, 1]
 
-    fib_sequence = [0]
+        while len(fib_numbers) < n:
+            next_fib = fib_numbers[-1] + fib_numbers[-2]
+            fib_numbers.append(next_fib)
 
-    if n == 1:
-        return fib_sequence
+        return fib_numbers
 
-    fib_sequence.append(1)
 
-    while len(fib_sequence) < n:
-        next_fib = fib_sequence[-1] + fib_sequence[-2]
-        fib_sequence.append(next_fib)
+if __name__ == "__main__":
 
-        return fib_sequence
+    print(fibonacci_sequence(6))
+    print(fibonacci_sequence(1))
+    print(fibonacci_sequence(0))
+    print(fibonacci_sequence(20))
--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,15 +1,22 @@
 def is_prime(number):
     if number <= 1:
         return False
+    elif number == 2:
+        return True
+    elif number % 2 == 0:
+        return False
 
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
+    max_divisor = int(number ** 0.5) + 1
+
+    for divisor in range(3, max_divisor, 2):
+        if number % divisor == 0:
             return False
 
         return True
 
 
-(is_prime(17))
-(is_prime(15))
-(is_prime(-5))
-(is_prime(0))
+if __name__ == "__main__":
+    print(is_prime(17))
+    print(is_prime(15))
+    print(is_prime(-5))
+    print(is_prime(0))
