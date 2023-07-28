c31b219498dd08fa1757602c223263d0f4351713
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-28 12:26:25 +0100

    task 1 modification

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,23 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
-    # Get the number of rows and columns in the matrix
-    num_rows = len(matrix)
-    num_cols = len(matrix[0]) if num_rows > 0 else 0
-
-    # Iterate through each row and print the elements with the specified format
     for row in matrix:
-        for col_idx, value in enumerate(row):
-            # Print each element with the specified format and separate them with spaces
-            print("{:d}".format(value),
-                  end=" " if col_idx < num_cols - 1 else "")
-        # Move to the next row after printing all elements in the current row
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d}".format(num), end="")
         print()
-
-
-# Test the function with an example matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
