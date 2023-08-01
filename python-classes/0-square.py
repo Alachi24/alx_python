778847fb378ea7ae935354c4781a37f0425aed1f
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-01 04:16:54 +0100

    modification

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,16 @@
 
 class Square:
 
-    """using a double underscore with a name after a function makes it private   """
+    """
+    using a double underscore with a name after a function makes it private.
+
+    """
 
     def __init__(self, size):
         self.__size = size
 
     def area(self):
         return self.__size ** self.__size
+
+
+print(Square.__doc__)
