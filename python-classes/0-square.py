e8c583f26ba31de3c0f43d3fdc9450461b883739
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-01 22:59:46 +0100

    modification

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,23 @@
 
 class Square:
-
-    """
-    using a double underscore with a name after a function makes it private.
-
-    """
+    """This a class considered as a parent to all instances being made in this module """
 
     def __init__(self, size):
+        """using a double underscore with a name after a function makes it private."""
         self.__size = size
 
-    def area(self):
-        return self.__size ** self.__size
 
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+    """A function to a method showing the attribute my_square while raising an exception"""
+    try:
+        print(my_square.size)
+    except AttributeError as e:
+        print(e)
 
-print(Square.__doc__)
+    try:
+        print(my_square.__size)
+    except AttributeError as e:
+        print(e)
