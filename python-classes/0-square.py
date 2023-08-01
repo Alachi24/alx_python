db52aa16a19ba7d9241d3b81c22c0d7f80c4e923
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-01 03:37:06 +0100

    modification

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,4 +10,17 @@ class Square:
         return self.__size ** self.__size
 
 
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
 print(Square.__doc__)
