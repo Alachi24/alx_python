526b8cc0ea25af2bd080b5da5026b67e86a1d166
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-07 01:15:59 +0100

    Task 0 success

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+class Base:
+    """
+    Creating our first class.
+    """
+    # A private class
+    __nb_objects = 0
+
+
+def __init__(self, id=None):
+    self.id = None
+
+    if id is not None:
+        self.id = id
+    else:
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
