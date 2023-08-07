4e8caccfb0f9d123b5ac8a5f30e4a9b7cd1522dc
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-07 01:25:24 +0100

    modify

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,13 +2,18 @@ class Base:
     """
     Creating our first class.
     """
-    # A private class
+    """
+     A private class
+     """
     __nb_objects = 0
 
 
 def __init__(self, id=None):
     self.id = None
 
+    """
+     Initialize instance with id.
+    """
     if id is not None:
         self.id = id
     else:
