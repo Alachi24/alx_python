44e15d0676030ca36001225e2326124d74cf765a
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-10 22:45:35 +0100

    modification

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,43 @@
+# TASK 0
+
+"""
+Defining a class that has both private and instances
+"""
+
+
+from base import Base
+
+
+class Base:
+    """
+    Creating our first class.
+    """
+    """
+     A private class
+     """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        function __init__ calling self and an id
+        """
+        self.id = None
+
+        """
+        Initialize instance with id.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
 
 # TASK 1
 
 """
 The class rectangle is inheriting from class Base
 """
-from base import Base
 
 
 class Rectangle(Base):
