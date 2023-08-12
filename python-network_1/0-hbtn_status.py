88bb61a91d572c26bb30feeb218b1815d2d4fd17
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-12 15:38:35 +0100

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,6 +7,8 @@ import requests
 rename it to 'req'
 """
 req = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
 # when ascertaining the type, you use this code to get <class 'str'>
 print("- type: {}".format(type(req.text)))
 # when ascertaining the text, you use this code to get 'ok'
