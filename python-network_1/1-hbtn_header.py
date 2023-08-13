cfbaac25307e2eea445a2386a220fca9c9c184bd
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-13 02:34:32 +0100

    sorted

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,7 +5,12 @@ you need the 'sys' & 'requests' for this task
 import requests
 import sys
 
-req = requests.get("https://intranet.hbtn.io")
+
+url = sys.argv[1]
+
+req = requests.get(url)
+print(req.headers.get('X-Request-Id'))
+
 
 # print(req.headers)  # it will show list of things available
-print(req.headers["X-Request-Id"])
+# print(req.headers["X-Request-Id"])
