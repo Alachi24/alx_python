4ea1f6bb8882830d4dfb64dbe6f29fb965b33e6d
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-23 14:59:02 +0100

    task 2 sorted

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+# imported these packages for use
+import MySQLdb
+import sys
+
+# created a variable
+database = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+
+cursor = database.cursor()
+state_name = sys.argv[4]
+
+cursor.execute(
+    "SELECT id, name FROM states WHERE name = %s ORDER BY id ASC", (state_name))
+states = cursor.fetchall()
+
+for state in states:
+    print(state)
+
+cursor.close()
+database.close()
