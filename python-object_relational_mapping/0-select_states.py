06f96631bbb0090538001e874e4caded1ba70990
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-23 11:03:39 +0100

    modified task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,9 @@ import sys
 
 # create a variable
 database = MySQLdb.connect(host="localhost", port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
 
 # create a 'cursor', note it can be named anything
 cursor = database.cursor()
