999deb8786342b1a53208a9585a97714632d0f38
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-23 10:36:52 +0100

    Task 0 sorted

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,17 @@
 # listing out all tables in a certain database
 import MySQLdb
 
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa
+# create a variable
+database = MySQLdb.connect(host="localhost", port=3306,
+                           user="Aloni", passwd="password", db="hbtn_0e_0_usa")
 
-#create a variable
-database = MySQLdb.connect(host = "localhost", port = 3306, user= "Aloni", passwd = "password", db = "hbtn_0e_0_usa")
-
-#create a 'cursor', note it can be named anything
+# create a 'cursor', note it can be named anything
 cursor = database.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id))");
-
-state = ("California", "Arizona", "Texas", "Arizona", "Nevada")
+cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+states = cursor.fetchall()
 
 for state in states:
-    cursor.execute("INSERT INTO states (name) VALUES (%s)", states)
+    print(state)
+
+cursor.close()
+database.close()
