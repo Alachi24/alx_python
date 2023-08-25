b6b7a5d79ad4c0f9e9ec3215f734c97ef5b03f49
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-25 10:37:05 +0100

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state = sys.argv[4]
 
     # created a variable that establishes connection to MySQL server
     database = MySQLdb.connect(host="localhost", port=3306,
@@ -19,8 +20,6 @@ if __name__ == '__main__':
               JOIN states ON cities.state_id = states.id\
               WHERE states.name = %s\
               ORDER BY cities.id ASC"
-
-    state = sys.argv[4]
 
     # executing a query
     cursor.execute(query, (state, ))
