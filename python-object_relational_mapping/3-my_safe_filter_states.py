e7130b13c2fda78c1fce11bba48a51571538fad1
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-25 10:02:14 +0100

    modification

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,13 +2,13 @@
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # created a variable
+    # created a variable that establishes connection to MySQL server
     database = MySQLdb.connect(host="localhost", port=3306,
                                user=username,
                                passwd=password,
@@ -16,16 +16,20 @@ if __name__ == "__main__":
 
     cursor = database.cursor()
     query = "SELECT id, name FROM states \
-            WHERE BINARY name = %s \
+            WHERE BINARY name =%s \
             ORDER BY id ASC"
 
     state_name = sys.argv[4]
 
-    cursor.execute = (query, (state_name, ))
+    # executing a query with the provided state_name
+    cursor.execute(query, (state_name, ))
+
+    # fetch all the matching rows
     states = cursor.fetchall()
 
     for state in states:
         print(state)
 
+    # close both cursor and database connection
     cursor.close()
     database.close()
