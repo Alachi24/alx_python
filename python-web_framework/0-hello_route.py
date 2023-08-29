f8bb281db7cf1832ee054860c9f88b12803daa3a
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-29 16:22:06 +0100

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -16,3 +16,7 @@ this is a decorator used to show flask what URL it'll trigger
 @app.route("/", strict_slashes=False)
 def hello():
     return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
