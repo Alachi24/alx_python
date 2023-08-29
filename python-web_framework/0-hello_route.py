3240fa4254af0b136811d97001d88261bda68bf2
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-29 15:12:55 +0100

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,12 +1,11 @@
-# importing the module flask
-from flask import Flask
+# importing from flask
+from flask import Flask  # Flask being imported
 
 # this is needed so flask knows where to look up resources
 app = Flask(__name__)
 
+
 # this is a decorator used to show flask what URL it'll trigger
-
-
 @app.route("/", strict_slashes=False)
 def hello():
     return "<p>Hello HBNB!</p>"
