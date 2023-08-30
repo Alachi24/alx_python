598d09ac27e303d3e6fc0ba030c33c7557843f62
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-30 11:02:04 +0100

    C is fun!

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -26,7 +26,7 @@ def hbnb():
 
 @app.route("/c/<text>/", strict_slashes=False)
 def c(text):
-    return f"C + {text.replace('_', ' ')}"
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == "__main__":
