af389503f1616596246e1252313367e5d9f4d629
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-10-06 21:32:31 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,7 +1,6 @@
-import csv
-import json
 import requests
 import sys
+import json
 
 if len(sys.argv) != 2:
     print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
@@ -10,9 +9,9 @@ if len(sys.argv) != 2:
 employee_id = sys.argv[1]
 url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 response = requests.get(url)
-employee_name = response.json().get("username")
+employee_data = response.json()
 
-if not employee_name:
+if "id" not in employee_data:
     print(f"No employee found with ID {employee_id}")
     sys.exit(1)
 
@@ -20,26 +19,23 @@ url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 response = requests.get(url)
 todos = response.json()
 
+employee_name = employee_data["username"]
+
 total_tasks = len(todos)
 done_tasks = sum(1 for todo in todos if todo.get("completed"))
 
-print(
-    f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-for todo in todos:
-    if todo.get("completed"):
-        print(f"\t {todo.get('title')}")
-
-# Export to JSON
-json_data = {employee_id: []}
-for todo in todos:
-    json_data[employee_id].append({
-        "task": todo.get("title"),
-        "completed": todo.get("completed"),
+# Prepare the data in JSON format
+data = {
+    "USER_ID": [{
+        "task": todo["title"],
+        "completed": todo["completed"],
         "username": employee_name
-    })
+    } for todo in todos]
+}
 
-json_filename = f'{employee_id}.json'
-with open(json_filename, 'w') as jsonfile:
-    json.dump(json_data, jsonfile)
+# Write the JSON data to a file
+filename = f"{employee_id}.json"
+with open(filename, "w") as json_file:
+    json.dump(data, json_file, indent=4)
 
-print(f"Data exported to {json_filename}")
+print(f"Data exported to {filename}")
